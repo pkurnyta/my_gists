f6bdd62c36a79711895974ed93d4bd91e59aa73e
pkurnyta <p.kurnyta@gmail.com>
2014-04-01 16:15:42 +0200

    rusztowanie + pygments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyGists::Application.config.secret_key_base = 'de696d97a787bb672012323c9d478f46f3d52c47b698452ff1e13cf1fec950147e9e1d51590010d46c90812f16346a9fcf1a6b6e0e24baa42a66b06702d5991a'
