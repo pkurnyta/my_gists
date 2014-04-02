e70734aa990d9d14d07fa7b582d2b7bd070f1dfc
Szymon <leyas@wp.pl>
2014-04-02 14:13:19 +0200

    dodałem rekordy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,203 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Gist.create(
+snippet:
+'
+//Hello World in C#
+class HelloWorld
+{
+static void Main()
+{
+System.Console.WriteLine("Hello, World!");
+}
+}
+',
+lang: 'c#',
+description: 'Hello World w C#'
+)
+
+Gist.create(
+snippet:
+'
+#include <iostream.h>
+
+main()
+{
+cout << "Hello World!" << endl;
+return 0;
+}
+',
+lang: 'c++',
+description: 'Hello World w C++'
+)
+
+Gist.create(
+snippet:
+'
+body:before {
+content: "Hello World";
+}
+',
+lang: 'css',
+description: 'Hello World w CSS'
+)
+
+Gist.create(
+snippet:
+"
+Program Hello_World;
+
+{$APPTYPE CONSOLE}
+
+Begin
+WriteLn('Hello World');
+End.
+",
+lang: 'delphi',
+description: 'Hello World w Delphi'
+)
+
+Gist.create(
+snippet:
+'
+<HTML>
+<!-- Hello World in HTML -->
+<HEAD>
+<TITLE>Hello World!</TITLE>
+</HEAD>
+<BODY>
+Hello World!
+</BODY>
+</HTML>
+',
+lang: 'html',
+description: 'Hello World w HTMLu'
+)
+
+Gist.create(
+snippet:
+'
+class HelloWorld {
+static public void main( String args[] ) {
+System.out.println( "Hello World!" );
+}
+}
+',
+lang: 'java',
+description: 'Hello World w Javie'
+)
+
+Gist.create(
+snippet:
+"
+{Hello World in Pascal}
+
+program HelloWorld(output);
+begin
+WriteLn('Hello World!');
+end.
+",
+lang: 'pascal',
+description: 'Hello World w Pascalu'
+)
+
+Gist.create(
+snippet:
+'
+# Hello world in perl
+
+print "Hello World!\n";
+',
+lang: 'perl',
+description: 'Hello World w Perlu'
+)
+
+Gist.create(
+snippet:
+"
+<?php
+// Hello World in PHP
+echo 'Hello World!';
+?>
+",
+lang: 'php',
+description: 'Hello World w PHP'
+)
+
+Gist.create(
+snippet:
+"
+# Hello World in Microsoft Powershell
+
+'Hello World!'
+",
+lang: 'powershell',
+description: 'Hello World w Microsoft Powershell'
+)
+
+Gist.create(
+snippet:
+'
+# Hello World in Python
+print "Hello World"
+',
+lang: 'python',
+description: 'Hello World w Pythonie'
+)
+
+Gist.create(
+snippet:
+'
+# Hello World in Ruby
+puts "Hello World!"
+',
+lang: 'ruby',
+description: 'Hello World w Ruby'
+)
+
+Gist.create(
+snippet:
+"
+# Hello World in SQL
+
+SELECT 'Hello World';
+",
+lang: 'sql',
+description: 'Hello World w SQL'
+)
+
+Gist.create(
+snippet:
+'
+// Hello World in Scala
+
+object HelloWorld with Application {
+Console.println("Hello world!");
+}
+',
+lang: 'scala',
+description: 'Hello World w Scala'
+)
+
+Gist.create(
+snippet:
+'
+% Hello World in plain \TeX
+\immediate\write16{Hello World!}
+\end
+',
+lang: 'tex',
+description: 'Hello World w TeX'
+)
+
+Gist.create(
+snippet:
+'
+#include <stdio.h>
+int main(void)
+{
+printf("Hello, world!\n");
+return 0;
+}
+',
+lang: 'c',
+description: 'Hello World w C'
+)
